12cbc2f0cf6f985f9bfaaffd18ee3737530acbab
Marcin <lempart.mar@gmail.com>
2022-10-27 13:38:44 +0200

    update.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'trafficmanagement'`, () => {
+  it(`should have as title 'psiinstagram'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('trafficmanagement');
+    expect(app.title).toEqual('psiinstagram');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('trafficmanagement app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('psiinstagram app is running!');
   });
 });
